21bdb260502512cb9fb6f6e2d33b3a0c92f7e2d5
Fab <fabianjarrett@gmail.com>
2020-09-16 19:12:53 +0100

    Add include guard

--- level.h
+++ level.h
@@ -1,3 +1,6 @@
+#ifndef LEVEL_H
+#define LEVEL_H
+
 struct Level
 {
     int tiles[100];
@@ -7,3 +10,5 @@ struct Level
 struct Level level_create(char *level_file);
 int level_get_tile(struct Level *level, int x, int y);
 int level_set_tile(struct Level *level, int x, int y, int tile_id);
+
+#endif
