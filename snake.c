fbd460a6695ae749003520a38210f83d8db13370
Fab <fabianjarrett@gmail.com>
2020-09-25 18:04:59 +0100

    Move comment to header

--- snake.c
+++ snake.c
@@ -24,7 +24,6 @@ bool snake_move(Snake *snake, int x, int y, Level *level)
     return true;
 }
 
-// Recursively find an adjacent tile of the same snake body and set it as the snake's child.
 void snake_find_bodies(Snake *snake, Snake snake_bodies[], Level *level)
 {
     const int body_tile_id = snake->tile_id + BODY_TILE_OFFSET;
--- snake.h
+++ snake.h
@@ -10,6 +10,8 @@ typedef struct Snake
     struct Snake *child;
 } Snake;
 bool snake_move(Snake *snake, int x, int y, Level *level);
+
+// Recursively find an adjacent tile of the same snake body and set it as the snake's child.
 void snake_find_bodies(Snake *snake, Snake snake_bodies[], Level *level);
 
 #endif
